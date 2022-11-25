4e30522f98b21c181728f1301383c8b72992322c
Ghosty2004 <fejerszilveszter@gmail.com>
2022-11-25 20:23:10 +0200

    Deleted

--- pages/api/search.ts
+++
@@ -1,7 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-
-export default function handler(request: NextApiRequest, response: NextApiResponse) {
-    if(request.method !== "GET") return response.status(405);
-    const { query } = request.query;
-    response.send(query);
-};
