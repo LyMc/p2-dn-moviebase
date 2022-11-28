821563c807c09c5cfb7ae2787dc9891e4bbb596e
Pope <andreipope16@gmail.com>
2022-11-28 23:17:14 +0200

    first test

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
-import { Center, Heading } from '@chakra-ui/react';
-import Layout from 'components/Layout';
+import { Center, Heading } from "@chakra-ui/react";
+import Layout from "components/Layout";
 
 export default function Home() {
   return (
     <Layout title="Moviebase">
       <Center h="full">
-        <Heading as="h2">Moviebase is up and running</Heading>
+        <Heading as="h2">Moviebase is Up and running</Heading>
       </Center>
     </Layout>
   );
